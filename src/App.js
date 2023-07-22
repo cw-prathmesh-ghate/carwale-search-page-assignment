5967e909d780ad6ae931ff1f9e10a78b9d98fc42
cw-prathmesh-ghate <113418779+cw-prathmesh-ghate@users.noreply.github.com>
2023-07-22 16:33:31 +0530

    created my react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./component/Navbar";
+import "./style.css"
+
+function App() {
+  return (
+    <>
+   <Navbar/>
+   <h1>hi I am prathmes</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import "../style.css"
+
+export default function(){
+    return (
+        <>
+        <div className="navbar-component">
+            <h1>I am navbar</h1>
+        </div>
+        </>
+    );
+}
